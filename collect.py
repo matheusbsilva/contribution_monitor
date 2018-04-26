6ce4259cc2bcbf735edf42ca3a5197a8a8a35658
matheusbsilva <matheusbattista@hotmail.com>
2018-04-26 18:51:02 -0300

    Export results to csv file

--- collect.py
+++ collect.py
@@ -5,6 +5,8 @@ Collect commits of each user on github graphql API
 import datetime
 import requests
 import os
+import json
+import pandas
 
 from dotenv import find_dotenv
 from dotenv import load_dotenv
@@ -16,10 +18,10 @@ load_dotenv(find_dotenv())
 TOKEN = "Bearer {token}".format(token=os.getenv('TOKEN'))
 HEADERS = {"Authorization": TOKEN}
 
-repo = "2018.1-TropicalHazards-BI"
-
 
 def run_query(query):
+    # TODO: Probably is the parsing of the query string to json that is taking too long 
+    # 2-5 seconds
     request = requests.post('https://api.github.com/graphql',
                             json={'query': query}, headers=HEADERS)
     if request.status_code == 200:
@@ -39,9 +41,7 @@ def get_collabs():
           }
         }
       }
-    }
-
-    """
+    } """
     raw_collabs = run_query(query_collabs)
     collabs = []
 
@@ -89,7 +89,6 @@ def get_commits(weekday):
     collabs = get_collabs()
     day = get_week_day(weekday)
     result = {}
-
     for collab in collabs:
         print("Collecting commits of {} on {}".format(collab[0], day))
         number = 0
@@ -111,22 +110,26 @@ def get_commits(weekday):
             """ % {'branch': branch, 'author': collab[1], 'date': day}
             response = run_query(query)
             number += response["data"]["repository"]["ref"]["target"]["history"]["totalCount"]
-        result[collab[0]] = number
 
+        result[collab[0]] = number
     return result
 
 
 def get_commits_of_week():
-    weekdays = (MO, TU)
+    weekdays = (MO, TU, WE)
     result = {}
 
     for weekday in weekdays:
-        result[weekday] = get_commits(weekday)
+        result[str(weekday)] = get_commits(weekday)
 
     return result
 
 
+def write_json_to_csv(result_dict):
+    df = pandas.DataFrame.from_dict(result_dict, orient='index')
+    return df.to_csv('data.csv', sep='\t', encoding='utf-8')
 
-# result = get_commits()
+
 result = get_commits_of_week()
-print(result)
+write_json_to_csv(result)
+
